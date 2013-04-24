823c9eb80884b03efad0c9e4fcc9478283dbfb90
Masha Rikhter <masha.rikhter@gmail.com>
2013-04-24 09:54:39 -0400

    add before filter redirect for naked url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   helper_method :current_user  
-    
+  
+  before_filter :add_www
+
+  def add_www
+    if request.env["HTTP_HOST"] == "totnnyc2013.com"
+      redirect_to "http://www.totnnyc2013.com/"
+    end
+  end
+
+
   private  
   def current_user  
     @current_user ||= User.find(session[:user_id]) if session[:user_id]  
