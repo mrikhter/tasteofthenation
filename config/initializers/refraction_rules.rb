f1a0e7c4bfebd34df60ef7cfa9e9d5b7ce2fbcc0
Masha Rikhter <masha.rikhter@gmail.com>
2013-04-24 11:00:18 -0400

    att http to refraction config

--- config/initializers/refraction_rules.rb
+++ config/initializers/refraction_rules.rb
@@ -1,5 +1,5 @@
 Refraction.configure do |req|
   if req.host == "totnnyc2013.com"
-    req.permanent! :host => "www.totnnyc2013.com"
+    req.permanent! :host => "http://www.totnnyc2013.com"
   end
 end
